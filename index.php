7515b9e74e6670ba101d48eb25e6eefb19a6029d
Eddy Martin <eddypmartin@gmail.com>
2022-09-13 16:44:33 -0400

    début du thème simple

---
+++ index.php
@@ -0,0 +1,44 @@
+
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package underscore
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+    <?php
+		if ( have_posts() ) :
+            while ( have_posts() ) :
+				the_post(); 
+                the_title();
+                the_content();
+            endwhile;
+        endif;
+    ?>    
+    </main>    
+</body>
+</html>
+
+
+
+
+
+
