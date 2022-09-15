d265327b556e3076dd8981809595aa76e005a021
Eddy Martin <eddypmartin@gmail.com>
2022-09-15 11:27:48 -0400

    modification wp_enqueue_style()

--- functions.php
+++ functions.php
@@ -48,7 +48,17 @@ add_action( 'after_setup_theme', 'underscore_setup' );
  * Enqueue scripts and styles.
  */
 function underscore_scripts() {
-	wp_enqueue_style( 'underscore-style', get_stylesheet_uri(), array(), _S_VERSION );
+	/*
+	wp_enqueue_style( 'underscore-style',
+					   get_stylesheet_uri(), 
+					   array(),
+					_S_VERSION );
+	*/				
+
+	wp_enqueue_style('underscore-style',
+					 get_template_directory_uri() . '/style.css',
+					 array(), 
+					 filemtime(get_template_directory() . '/style.css'), false);
 	
 }
 add_action( 'wp_enqueue_scripts', 'underscore_scripts' );
