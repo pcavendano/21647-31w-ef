f3b1fd5883d9c31fcfe405cb97e886509bb58976
Eddy Martin <eddypmartin@gmail.com>
2022-09-15 10:31:28 -0400

    modifier the_title() en ajoutant un h1  autour du titre

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
 		if ( have_posts() ) :
             while ( have_posts() ) :
 				the_post(); 
-                the_title('<p>','</p>');
+                the_title('<h1>','</h1>');
                 the_content(null, true);
             endwhile;
         endif;
