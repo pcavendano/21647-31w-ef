87467a9becc5fc15f9281581536ed3609856422b
Eddy Martin <eddypmartin@gmail.com>
2022-09-27 16:01:17 -0400

    ajout de front-page.php et single.php

---
+++ front-page.php
@@ -0,0 +1,38 @@
+
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package underscore
+ */
+?>
+<h1 class="trace">front-page.php</h1>
+<?php get_header(); ?>
+
+    <main>
+    <?php
+		if ( have_posts() ) :
+            while ( have_posts() ) :
+				the_post(); ?>
+                <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+                <?php the_content(null, true); ?>
+                
+            <?php endwhile; ?>
+        <?php endif; ?>
+    ?>    
+    </main>    
+<?php get_footer(); ?>
+</html>
+
+
+
+
+
+
---
+++ single.php
@@ -0,0 +1,60 @@
+
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package underscore
+ */
+?>
+<h1 class="trace">single.php</h1>
+<?php get_header(); ?>
+
+    <main>
+    <?php
+		if ( have_posts() ) :
+            while ( have_posts() ) :
+				the_post(); 
+                the_title('<h2>','</h2>');
+                the_content(null, true);?>
+                <section>
+                    <small>
+                        <span><?php the_weekday(); ?>
+                    </span> 
+                        
+                        <span><?php the_date(); ?></span> 
+                        
+                        <span><?php the_time(); ?></span>  
+                        
+                    </small>
+                    <code><?php the_author(); ?> 
+                         
+                    </code>
+                    <pre><?php the_category();  ?>
+                         
+                    </pre>
+                </section>
+                <?php 
+               
+               
+                
+               
+            endwhile;
+        endif;
+    ?>    
+    </main>  
+  
+<?php get_footer(); ?>
+</html>
+
+
+
+
+
+
