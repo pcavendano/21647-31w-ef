2fedf057cafb8b6dc43d3c845811b127b40365c4
Eddy Martin <eddypmartin@gmail.com>
2022-10-25 14:49:41 -0400

    c13 debut de la structure widget/sidebar

--- functions.php
+++ functions.php
@@ -95,3 +95,23 @@ function igc31w_filtre_choix_menu($obj_menu){
 
 }
 add_filter("wp_nav_menu_objects","igc31w_filtre_choix_menu");
+
+
+/* -------------------------------------------------------- Initialisation des sidebar */
+
+add_action( 'widgets_init', 'my_register_sidebars' );
+function my_register_sidebars() {
+	/* Register the 'primary' sidebar. */
+	register_sidebar(
+		array(
+			'id'            => 'primary',
+			'name'          => __( 'Primary Sidebar' ),
+			'description'   => __( 'A short description of the sidebar.' ),
+			'before_widget' => '<div id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</div>',
+			'before_title'  => '<h3 class="widget-title">',
+			'after_title'   => '</h3>',
+		)
+	);
+	/* Repeat register_sidebar() code for additional sidebars. */
+}
