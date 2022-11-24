15c6d9405aa68eb09f030497c00c3a09b46a6c75
Pedro Contreras Avendano <pedro.contreras.avendano@gmail.com>
2022-11-24 10:02:01 -0500

    ef - Debut des widgets, enregistrement des sidebars

---
+++ sidebar-header-1.php
@@ -0,0 +1,3 @@
+<div id="sidebar-header-1" class="sidebar">
+	<?php dynamic_sidebar( 'header-1' ); ?>
+</div>
---
+++ sidebar-header-2.php
@@ -0,0 +1,3 @@
+<div id="sidebar-header-2" class="sidebar">
+	<?php dynamic_sidebar( 'header-2' ); ?>
+</div>
